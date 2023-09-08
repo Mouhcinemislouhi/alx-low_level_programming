caf85fe857b7fb944465758638a7e2ac86e2073f
Mouhcinemislouhi <mohcinmislohi@gmail.com>
2023-09-08 06:09:19 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,25 +5,25 @@
 /**
  * main - Entry point for alphabet printing program
  *
- * This program prints the lowercase and uppercase alphabet
- * in sequence, followed by a newline character.
+ * Description: This program prints the lowercase and uppercase alphabet
+ *              in sequence, followed by a newline character.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-    for (letter = 'A'; letter <= 'Z'; letter++)
-    {
-        putchar(letter);
-    }
-    putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
